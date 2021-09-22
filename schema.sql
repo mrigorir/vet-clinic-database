481db6f11952cd7b1fa7a0b6a3debf2febbbacc4
eroauditore <alfa.fenrir@gmail.com>
2021-09-22 15:42:24 -0500

    created data and added an index for animal_id

--- schema.sql
+++ schema.sql
@@ -48,5 +48,8 @@ CREATE TABLE visits(
 
 
 INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
-
-INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
+​INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+CREATE INDEX animal_index ON visits (animal_id);
